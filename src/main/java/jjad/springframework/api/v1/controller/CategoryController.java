13d845369034e48743519522d61228dd42fc13fd
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-04 13:57:28 -0600

    adding category cnontroller

---
+++ src/main/java/jjad/springframework/api/v1/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package jjad.springframework.api.v1.controller;
+
+import jjad.springframework.api.v1.model.CategoryDTO;
+import jjad.springframework.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping({"/",""})
+    public ResponseEntity<List<CategoryDTO>> getAllCategories(){
+        return new ResponseEntity<List<CategoryDTO>>(
+                categoryService.getAllCategories(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name){
+        return new ResponseEntity<CategoryDTO>(categoryService.getCategoryByName(name), HttpStatus.OK);
+    }
+}
---
+++ src/test/java/jjad/springframework/api/v1/controller/CategoryControllerTest.java
@@ -0,0 +1,83 @@
+package jjad.springframework.api.v1.controller;
+
+import jjad.springframework.api.v1.model.CategoryDTO;
+import jjad.springframework.services.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+class CategoryControllerTest {
+
+    public static final String NAME = "category1";
+    @Mock
+    CategoryService categoryService;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @Test
+    void getAllCategories() throws Exception {
+        //given
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1L);
+        category1.setName(NAME);
+
+        CategoryDTO category2 = new CategoryDTO();
+        category2.setId(2L);
+        category2.setName("JJ");
+
+        List<CategoryDTO> categories = Arrays.asList(category1, category2);
+
+        //when
+        when(categoryService.getAllCategories()).thenReturn(categories);
+
+        //then
+        mockMvc.perform(get("/api/v1/categories")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    void getCategoryByName() throws Exception{
+        //given
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1L);
+        category1.setName(NAME);
+
+        //when
+        when(categoryService.getCategoryByName(anyString())).thenReturn(category1);
+
+        //then
+        mockMvc.perform(get("/api/v1/categories/" + NAME)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(NAME)));
+    }
+}

7ca11d5460f8cc1c2f225530e4cd80ebcc71f646
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-03 21:29:33 -0600

    init commit

---
+++ src/test/java/jjad/springframework/SpringMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package jjad.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

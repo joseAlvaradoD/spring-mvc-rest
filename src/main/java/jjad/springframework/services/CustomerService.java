12b1e96cb2e7808215d3d0cb45ad8152c33e4694
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-04 18:22:34 -0600

    adding put method on customers endpoint

--- src/main/java/jjad/springframework/services/CustomerService.java
+++ src/main/java/jjad/springframework/services/CustomerService.java
@@ -8,6 +8,6 @@ public interface CustomerService {
 
     List<CustomerDTO> getAllCustomers();
     CustomerDTO getCustomerById(Long id);
-    CustomerDTO createNewCustomer(CustomerDTO customerDTO);
+    CustomerDTO saveCustomer(CustomerDTO customerDTO);
 
 }
